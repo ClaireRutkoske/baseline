5c60ca9dd9852e06f5b7446a5c64a396833d9c2b
clairerutkoske <claire.rutkoske@gmail.com>
2015-11-16 17:02:35 -0600

    Vertical, horizontal centering css

---
+++ css.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Baseline</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="dist/styles/main.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+				<div class="fullpage">
+					<div class="outer">
+						<div class="inner">
+							<p>Vertical Align</p>
+						</div>
+					</div>			
+				</div>
+
+        <script src="dist/scripts/jquery.js"></script>
+		<script src="dist/scripts/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script></script>
+    </body>
+</html>
